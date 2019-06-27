737b564249fdf3cad04f9326b911a4936ab53609
Shivani <shivani.chelliah@publicissapient.com>
2019-06-27 16:48:56 +0400

    install node packages

--- src/App.js
+++ src/App.js
@@ -8,16 +8,13 @@ import Home from './Home';
 function App() {
   return (
     <BrowserRouter>
-    <div>
-   
-      <Switch>
-        <Route exact path='/' component={Home} />
-        
-        <Route render={() => <h1>Page not found</h1>} />
-      </Switch>
-      
-    </div>
-  </BrowserRouter>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route render={() => <h1>Page not found</h1>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
