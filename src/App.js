5b7cde48ff55a6490602f345de2a055ff9f67a47
Shivani <shivani.chelliah@publicissapient.com>
2019-07-01 16:29:41 +0400

    add new route for meeting room stats

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Home from './Home';
+import MeetingRoomStats from './MeetingRoomStats';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/meeting-rooms-stats' component={MeetingRoomStats} />
           <Route render={() => <h1>Page not found</h1>} />
         </Switch>
       </div>
